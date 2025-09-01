d80227e684263d4b175528a99ebd4af85fdd3317
SansoTheBeast <sanskarthitame2006@gmail.com>
2025-09-01 15:08:16 +0530

    Add files via upload

---
+++ volume.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){ 
+  int a;
+  int h;
+  printf("the radius of circle is :");
+  scanf("%d",&a);
+  printf("the height of cylinder is :");
+  scanf("%d",&h);
+  printf("the radius of the circle is %f\n",3.14*a*a);
+  printf("the volume of the cylinder is %f\n",3.14*a*a*h);
+
+  return 0; 
+}
